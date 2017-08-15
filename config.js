20104f7f5f6ee46955feb6db25d0dff49b5cb899
Etxahun <etxahun.sanchez@gmail.com>
2017-08-15 22:41:25 +0200

    gitignore file updated

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  'consumer_key': 'MTme5qq5XSmt3URKf7feaNhKK',
-  'consumer_secret': 'tstGpqf9Lue0aatrDVY7Vua8ZpyAQF0oYFxMgBF5EuALZMItxO',
-  'access_token': '740303653542760449-V16Zj8ZcFTD1dFtZBZksQuYXYNX6mtA',
-  'access_token_secret': 'PvjdKDE91AHgHoccPvG6Q1333JX87lZnIy7VoEppVkP0u'
-}
